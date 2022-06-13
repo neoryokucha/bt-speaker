4fd301423008642ff5a1ef0c0a558e221033ffff
neoryokucha <97508326+neoryokucha@users.noreply.github.com>
2022-06-13 20:38:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -e
 
 echo "Installing dependencies..."
 apt update
-apt --yes install git bluez python3 python3-gi python3-cffi python3-dbus python3-alsaaudio python-configparser sound-theme-freedesktop vorbis-tools
+apt install git bluez python3 python3-gi python3-cffi python3-dbus python3-alsaaudio python-configparser sound-theme-freedesktop vorbis-tools
 echo "done."
 
 # Add btspeaker user if not exist already
